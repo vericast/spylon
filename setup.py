d2b20bea253ef8492646da5df363621c2c0568f3
mariusvniekerk <marius.v.niekerk@gmail.com>
2016-07-12 15:04:05 -0500

    Updated spark launcher to pull property helpers from json files.

--- setup.py
+++ setup.py
@@ -40,6 +40,14 @@ version = '%s'
 
 write_version_py()
 
+
+spark_versions = ["1.6.0", "1.6.1", "latest"]
+from spylon.spark.launcher import _save_documentation
+
+for sv in spark_versions:
+    _save_documentation(version=sv) 
+
+
 setup(name='spylon',
       version=version,
       packages=find_packages(),
