4cee635818199dff7fb2dcc596b614afedd4574b
Niels Reijers <nielsreijers@gmail.com>
2024-01-11 15:42:41 +0800

    Updated the code generated by versioneer to version 0.29
    The old version (generated by versioneer 0.17) used the SafeConfigParser in configparser, which had been deprecated for a while and remove in Python 3.12.
    
    See: https://docs.python.org/3/whatsnew/3.12.html#removed

--- spylon/__init__.py
+++ spylon/__init__.py
@@ -24,6 +24,5 @@
 
 from __future__ import print_function, absolute_import
 
-from ._version import get_versions
-__version__ = get_versions()['version']
-del get_versions
+from . import _version
+__version__ = _version.get_versions()['version']
